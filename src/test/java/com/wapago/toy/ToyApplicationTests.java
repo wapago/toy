fc718752774156b99a8a9a5eeaadab2e7e30df28
Kim Chanho <theanswerkk@naver.com>
2022-07-18 00:48:10 +0900

    Initial commit

---
+++ src/test/java/com/wapago/toy/ToyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wapago.toy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
